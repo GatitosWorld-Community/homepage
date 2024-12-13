e537886d690f421cc9a7ffe044c01380d0d1f434
yxsifdev <yosifblancotafur2006@gmail.com>
2024-12-13 15:05:28 -0500

    Update astroConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://gatitosworld-community.github.io",
-  base: "homepage",
   integrations: [tailwind()],
 });
